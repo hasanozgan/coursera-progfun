0f6e8acca9cbe9311bc4a9d427978e15eefadc1b
hasanozgan <hasanozgan@gmail.com>
2013-10-29 22:39:28 +0200

    coursera progfun assignments

---
+++ assignments/week1/recfun/src/main/scala/recfun/Main.scala
@@ -0,0 +1,42 @@
+package recfun
+import common._
+
+object Main {
+  def main(args: Array[String]) {
+    println("Pascal's Triangle")
+    for (row <- 0 to 10) {
+      for (col <- 0 to row)
+        print(pascal(col, row) + " ")
+      println()
+    }
+  }
+
+  /**
+   * Exercise 1
+   */
+  def pascal(c: Int, r: Int): Int =
+    if (c == 0 || c == r) 1
+    else pascal(c - 1, r - 1) + pascal(c, r - 1)
+
+  /**
+   * Exercise 2
+   */
+  def balance(chars: List[Char]): Boolean = {
+    def b(ch: List[Char], o: Int, lc: Boolean): Boolean = {
+      if (ch.isEmpty) o == 0 && lc
+      else if (ch.head == '(') b(ch.tail, o + 1, false)
+      else if (ch.head == ')') b(ch.tail, o - 1, true)
+      else b(ch.tail, o, lc)
+    }
+    b(chars, 0, true)
+  }
+
+  /**
+   * Exercise 3
+   */
+  def countChange(money: Int, coins: List[Int]): Int = {
+    if(money == 0) 1
+    else if (money < 0 || coins.isEmpty) 0
+    else countChange(money, coins.tail) + countChange(money - coins.head, coins)
+  }
+}
